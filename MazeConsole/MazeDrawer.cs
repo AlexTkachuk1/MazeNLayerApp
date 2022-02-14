5c9f2ccbb7f808330750ff042f67b0f32e3b6b6d
AlexTkachuk1 <Lyucifer97@list.ru>
2022-02-14 23:17:56 +0300

    Add MazeConsole for easy testing of BLL methods and demonstration of application performance on the console.

---
+++ MazeConsole/MazeDrawer.cs
@@ -0,0 +1,53 @@
+﻿using NLayerApp.BLL_.DTO.Cells;
+using NLayerApp.BLL_.DTO.Interfaces;
+
+namespace MazeConsole
+{
+    public class MazeDrawer
+    {
+        public void Draw(IMaze maze)
+        {
+            foreach (var cell in maze.CellsWithHero)
+            {
+                Console.SetCursorPosition(cell.CordinateX, cell.CordinateY);
+                if (cell is Wall)
+                {
+                    //Console.BackgroundColor = ConsoleColor.White;
+                    //Console.Write("U+2B1C",UTF32Encoding.Equals(U+2B1C));
+                    //Console.OutputEncoding = System.Text.Encoding.UTF8;
+                    Console.WriteLine("#");
+                    //Console.BackgroundColor = ConsoleColor.Black;
+                }
+                if (cell is Ground)
+                {
+                    Console.Write(".");
+                }
+                if (cell is GoldHeap)
+                {
+                    Console.Write("$");
+                }
+                if (cell is Сhest)
+                {
+                    Console.Write("?");
+                }
+                if (cell is CellWithHero)
+                {
+                    Console.Write("@");
+                }
+                if (cell is Gate)
+                {
+                    Console.Write("O");
+                }
+                if (cell is Lava)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Write(" ");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+
+            Console.SetCursorPosition(0, maze.Height + 1);
+        }
+    }
+}
+
---
+++ MazeConsole/Program.cs
@@ -0,0 +1,82 @@
+﻿using NLayerApp.BLL_.BusinessModels;
+using NLayerApp.BLL_.DTO;
+
+namespace MazeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mazeDrawer = new MazeDrawer();
+            //Type type = mazeDrawer.GetType();
+
+            //ShowTypeInfo(typeof(Maze));
+
+            //Console.ReadLine();
+            Maze();
+        }
+
+        public static void ShowTypeInfo(Type type)
+        {
+            Console.WriteLine(type.FullName);
+
+            Console.WriteLine("------ All Properties");
+            foreach (var propertyInfo in type.GetProperties())
+            {
+                Console.WriteLine($"{propertyInfo.PropertyType.FullName} {propertyInfo.Name}: ");
+            }
+
+            Console.WriteLine("------ All Methods");
+            foreach (var methodInfo in type.GetMethods())
+            {
+                Console.WriteLine($"{methodInfo.ReturnType.FullName} {methodInfo.Name} {methodInfo.GetParameters().Length} ");
+            }
+        }
+
+        public static void Maze()
+        {
+            var builder = new MazeBuilder();
+
+            var drawer = new MazeDrawer();
+
+            var maze = builder.Build(20, 10, drawer.Draw);
+
+            drawer.Draw(maze);
+
+            var exit = false;
+            while (!exit)
+            {
+                var gateCell = maze.Cells.Single(x => x.GetType().Name == "Gate");
+                if (maze.Hero.X == gateCell.CordinateX && maze.Hero.Y == gateCell.CordinateY)
+                {
+                    var newMaze = builder.Build(maze.Width, maze.Height, maze.DrawStepByStep);
+                    newMaze.Hero.Gold = maze.Hero.Gold;
+                    newMaze.Hero.Inventory = maze.Hero.Inventory;
+                    maze = newMaze;
+                }
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        maze.TryToStep(Direction.Up);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        maze.TryToStep(Direction.Right);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        maze.TryToStep(Direction.Down);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        maze.TryToStep(Direction.Left);
+                        break;
+                    case ConsoleKey.Escape:
+                        exit = true;
+                        break;
+                }
+                maze = builder.StateOfTheMaze();
+                drawer.Draw(maze);
+            }
+        }
+    }
+}
